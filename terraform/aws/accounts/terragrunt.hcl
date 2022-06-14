5f031842b8e88a516a6cd934ab4afdfb3e50a2da
Jacob Woffenden <jacob@woffenden.io>
2022-06-14 15:37:40 +0000

    Updated Terraform and Terragrunt versions Updated Terraform provider versions

--- terraform/aws/accounts/terragrunt.hcl
+++ terraform/aws/accounts/terragrunt.hcl
@@ -31,7 +31,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.14"
+      version = "~> 4.18"
     }
   }
 }
--- terraform/google/woffenden/mta-sts/terraform.tf
+++ terraform/google/woffenden/mta-sts/terraform.tf
@@ -6,11 +6,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 4.24"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.0"
+      version = "~> 3.16"
     }
   }
 }
