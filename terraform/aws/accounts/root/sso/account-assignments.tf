932fe1c7a0668f48a93eb0cd8bcebc6bffd98e71
Jacob Woffenden <jacob@woffenden.io>
2023-02-09 23:24:06 +0000

    Update SSO

--- terraform/aws/accounts/root/sso/account-assignments.tf
+++ terraform/aws/accounts/root/sso/account-assignments.tf
@@ -1,7 +1,7 @@
 resource "aws_ssoadmin_account_assignment" "development_aws_administrators" {
   instance_arn = tolist(data.aws_ssoadmin_instances.main.arns)[0]
 
-  permission_set_arn = aws_ssoadmin_permission_set.aws_administrator_access.arn
+  permission_set_arn = aws_ssoadmin_permission_set.administrator_access.arn
 
   principal_type = "GROUP"
   principal_id   = aws_identitystore_group.aws_administrators.group_id
--- terraform/aws/accounts/root/sso/permission-sets.tf
+++ terraform/aws/accounts/root/sso/permission-sets.tf
@@ -1,10 +1,10 @@
-resource "aws_ssoadmin_permission_set" "aws_administrator_access" {
+resource "aws_ssoadmin_permission_set" "administrator_access" {
   instance_arn = tolist(data.aws_ssoadmin_instances.main.arns)[0]
   name         = "AWSAdministratorAccess"
 }
 
-resource "aws_ssoadmin_managed_policy_attachment" "aws_administrator_access" {
+resource "aws_ssoadmin_managed_policy_attachment" "dministrator_access" {
   instance_arn       = tolist(data.aws_ssoadmin_instances.main.arns)[0]
-  permission_set_arn = aws_ssoadmin_permission_set.aws_administrator_access.arn
-  managed_policy_arn = "arn:aws:iam::aws:policy/AWSAdministratorAccess"
+  permission_set_arn = aws_ssoadmin_permission_set.administrator_access.arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
