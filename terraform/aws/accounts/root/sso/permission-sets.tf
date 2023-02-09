cdb8aebe03ff2b427e4cb666c4725e4275e41b1c
Jacob Woffenden <jacob@woffenden.io>
2023-02-09 23:19:06 +0000

    Fix account assignment and permission set

--- terraform/aws/accounts/root/sso/permission-sets.tf
+++ terraform/aws/accounts/root/sso/permission-sets.tf
@@ -5,6 +5,6 @@ resource "aws_ssoadmin_permission_set" "aws_administrator_access" {
 
 resource "aws_ssoadmin_managed_policy_attachment" "aws_administrator_access" {
   instance_arn       = tolist(data.aws_ssoadmin_instances.main.arns)[0]
-  permission_set_arn = aws_ssoadmin_permission_set.aws_administrator_access
+  permission_set_arn = aws_ssoadmin_permission_set.aws_administrator_access.arn
   managed_policy_arn = "arn:aws:iam::aws:policy/AWSAdministratorAccess"
 }
