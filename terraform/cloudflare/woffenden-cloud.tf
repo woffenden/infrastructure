89f16cff947a4d0c1ab89ee49292b46d5e0f4646
Jacob Woffenden <jacob@woffenden.io>
2023-02-09 23:34:10 +0000

    Commenting out zones, need importing first

--- terraform/cloudflare/woffenden-cloud.tf
+++ terraform/cloudflare/woffenden-cloud.tf
@@ -1,5 +1,5 @@
-module "woffenden_cloud_cloudflare_zone" {
-  source = "../modules/cloudflare/zone"
-  account = "af790e83102c7ab5347e7dfbf86ef021"
-  zone   = "woffenden.cloud"
-}
+# module "woffenden_cloud_cloudflare_zone" {
+#   source = "../modules/cloudflare/zone"
+#   account = "af790e83102c7ab5347e7dfbf86ef021"
+#   zone   = "woffenden.cloud"
+# }
