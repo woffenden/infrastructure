9f9de8a73c271f2627e0892c0d63d9243c810bc1
Jacob Woffenden <jacob@woffenden.io>
2023-02-09 23:09:53 +0000

    Fix permission set

--- terraform/aws/accounts/root/sso/permission-sets.tf
+++ terraform/aws/accounts/root/sso/permission-sets.tf
@@ -1,6 +1,6 @@
 resource "aws_ssoadmin_permission_set" "aws_administrator_access" {
+  instance_arn = tolist(data.aws_ssoadmin_instances.main.arns)[0]
   name         = "AWSAdministratorAccess"
-  instance_arn = tolist(data.aws_ssoadmin_instances.example.arns)[0]
 }
 
 resource "aws_ssoadmin_managed_policy_attachment" "aws_administrator_access" {
