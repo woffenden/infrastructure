52c28a070012b4b3a30d266a3ccb93eb11cf7aa5
Jacob Woffenden <jacob@woffenden.io>
2023-04-05 22:52:15 +0100

    Remove Cloudflare GitHub provider

--- terraform/cloudflare/access-applications.tf
+++ terraform/cloudflare/access-applications.tf
@@ -2,8 +2,3 @@ data "cloudflare_access_identity_provider" "google_workspace" {
   account_id = "af790e83102c7ab5347e7dfbf86ef021"
   name       = "woffenden.io"
 }
-
-data "cloudflare_access_identity_provider" "github" {
-  account_id = "af790e83102c7ab5347e7dfbf86ef021"
-  name       = "woffenden"
-}
