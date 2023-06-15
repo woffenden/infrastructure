828378dc78525fda54675b12e40ea6adbd56e6b9
Jacob Woffenden <jacob@woffenden.io>
2023-06-15 21:23:32 +0000

    Fixed formatting

--- terraform/google/woffenden/mta-sts/terraform.tf
+++ terraform/google/woffenden/mta-sts/terraform.tf
@@ -16,7 +16,7 @@ terraform {
 }
 
 provider "cloudflare" {
-  email = data.google_secret_manager_secret_version.cloudflare_email.secret_data
+  email   = data.google_secret_manager_secret_version.cloudflare_email.secret_data
   api_key = data.google_secret_manager_secret_version.cloudflare_api_key.secret_data
 }
 
