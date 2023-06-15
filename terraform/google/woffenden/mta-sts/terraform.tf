0bd5bbb59fd7e90618865c5dad892945f127ac38
Jacob Woffenden <jacob@woffenden.io>
2023-06-15 18:39:28 +0000

    Grab Cloudflare cred from Google Secret Manager

--- terraform/google/woffenden/mta-sts/terraform.tf
+++ terraform/google/woffenden/mta-sts/terraform.tf
@@ -15,7 +15,10 @@ terraform {
   }
 }
 
-provider "cloudflare" {}
+provider "cloudflare" {
+  email = data.google_secret_manager_secret_version.cloudflare_email.secret_data
+  api_key = data.google_secret_manager_secret_version.cloudflare_api_key.secret_data
+}
 
 provider "google" {
   project = "woffenden"
