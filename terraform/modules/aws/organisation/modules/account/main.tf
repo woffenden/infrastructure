ea079dcdba88453d0be2a4bd9f9e271df0c4117e
Jacob Woffenden <jacob@woffenden.io>
2023-06-16 03:07:57 +0100

    🚉 Create Core Platform Services account  (#37)

--- terraform/modules/aws/organisation/modules/account/main.tf
+++ terraform/modules/aws/organisation/modules/account/main.tf
@@ -4,6 +4,7 @@ resource "aws_organizations_account" "this" {
   email                      = local.email
   role_name                  = var.role_name
   iam_user_access_to_billing = var.iam_user_access_to_billing
+  close_on_deletion          = var.close_on_deletion
   lifecycle {
     ignore_changes = [
       name,
