facc57533fb9e25b86b89aaf96f490881bb543a1
Jacob Woffenden <jacob@woffenden.io>
2023-06-16 07:54:46 +0100

    🚑 Fix ECR (but this time my commit is signed) (#48)

--- terraform/aws/accounts/platform-services/ecr/locals.tf
+++ terraform/aws/accounts/platform-services/ecr/locals.tf
@@ -1,6 +1,6 @@
 locals {
   ecr_repositories = {
-    test = {
+    aws-ssosync = {
       name      = "aws-ssosync"
       push_arns = ["arn:aws:iam::749019155605:role/github-actions"]
       pull_arns = ["arn:aws:iam::655105654343:root"]
