24fc27608b1748475f5db96ca4d1364cd955bd00
Jacob Woffenden <jacob@woffenden.io>
2023-06-16 08:10:16 +0100

    🛂 Add Platform Services IAM (#49)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ terraform/aws/accounts/platform-services/iam/terraform.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "s3" {
+    bucket         = "woffenden-terraform"
+    key            = "platform-services/iam/terraform.tfstate"
+    dynamodb_table = "terraform"
+    encrypt        = true
+    kms_key_id     = "arn:aws:kms:eu-west-2:749019155605:key/e68a6c13-c02f-4bc2-bf92-03b0edf55b4d"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  assume_role {
+    role_arn = "arn:aws:iam::126246520815:role/organisation-administrator-role"
+  }
+  default_tags {
+    tags = {
+      "business-unit" = "platforms"
+      "application"   = "infrastructure"
+      "component"     = "iam"
+      "owner"         = "ddat.aws@woffenden.io"
+      "source-code"   = "github.com/woffenden/infrastructure/terraform/aws/accounts/platform-services/iam"
+      "environment"   = "management"
+      "is-production" = "true"
+      "managed-by"    = "terraform"
+    }
+  }
+}
