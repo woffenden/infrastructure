270635a916078b1c5d672d5f05250acc725637e1
Jacob Woffenden <jacob@woffenden.io>
2023-06-16 01:49:45 +0000

    Add Platform Services account

--- terraform/aws/accounts/root/organisation/accounts.tf
+++ terraform/aws/accounts/root/organisation/accounts.tf
@@ -1,3 +1,9 @@
+module "organisation_account_platform_services" {
+  source              = "../../../../modules/aws/organisation/modules/account"
+  name                = "Platform Services"
+  organisational_unit = module.organisational_unit_core.id
+}
+
 module "organisation_account_development" {
   source              = "../../../../modules/aws/organisation/modules/account"
   name                = "Development"
