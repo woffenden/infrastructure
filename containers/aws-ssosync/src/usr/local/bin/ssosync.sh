ff0a0abb5f942d9df684b692991693add523939e
Jacob Woffenden <jacob@woffenden.io>
2023-06-18 21:33:40 +0000

    Adding scan step Switch to multi-stage build Added wrapper script for ssosync

--- containers/aws-ssosync/src/usr/local/bin/ssosync.sh
+++ containers/aws-ssosync/src/usr/local/bin/ssosync.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+
+echo "Writing AWS_SSO_SYNC_GOOGLE_CREDENTIALS to credentials.json"
+cat "${AWS_SSO_SYNC_GOOGLE_CREDENTIALS}" > credentials.json
+
+echo "Running ssosync"
+/usr/local/bin/ssosync \
+  --log-level "${AWS_SSO_SYNC_LOG_LEVEL}" \
+  --region "${AWS_IDENTITY_STORE_REGION}" \
+  --identity-store-id "${AWS_IDENTITY_STORE_ID}" \
+  --access-token "${AWS_SSO_SYNC_SCIM_TOKEN}" \
+  --endpoint "${AWS_SSO_SYNC_SCIM_ENDPOINT}" \
+  --google-admin "${AWS_SSO_SYNC_GOOGLE_ADMIN}" \
+  --google-credentials credentials.json \
+  --sync-method "${AWS_SSO_SYNC_METHOD}"
