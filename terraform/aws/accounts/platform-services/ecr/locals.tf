11fd2b4d2710f84ac6f60d926844401e62ee1083
Jacob Woffenden <jacob@woffenden.io>
2023-06-19 00:09:38 +0000

    Updated ECR module

--- terraform/aws/accounts/platform-services/ecr/locals.tf
+++ terraform/aws/accounts/platform-services/ecr/locals.tf
@@ -2,8 +2,9 @@ locals {
   ecr_repositories = {
     aws-ssosync = {
       name      = "aws-ssosync"
-      push_arns = ["arn:aws:iam::749019155605:role/github-actions"]
-      pull_arns = ["arn:aws:iam::655105654343:root"]
+      pull_accounts = [
+        "655105654343" # Production
+      ]
     }
   }
 }
