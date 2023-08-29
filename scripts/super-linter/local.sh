19f94e13fb30ab6a1dde1d6b6f4d1a1a87299cff
Jacob Woffenden <jacob@woffenden.io>
2023-08-29 16:34:04 +0000

    Updated local linter script

--- scripts/super-linter/local.sh
+++ scripts/super-linter/local.sh
@@ -4,4 +4,4 @@ docker run --rm \
   --env RUN_LOCAL="true" \
   --env-file ".github/super-linter.env" \
   --volume "${PWD}":/tmp/lint \
-  ghcr.io/github/super-linter:v5.0.0
+  ghcr.io/super-linter/super-linter:slim-v5
