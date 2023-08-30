314ba683ae6dfbc839ac683b166895b0bfcaa68a
Jacob Woffenden <jacob@woffenden.io>
2023-08-30 09:24:59 +0000

    Add logging to local super-linter

--- scripts/super-linter/local.sh
+++ scripts/super-linter/local.sh
@@ -2,6 +2,9 @@
 
 docker run --rm \
   --env RUN_LOCAL="true" \
+  --env CREATE_LOG_FILE="true" \
+  --env LOG_FILE_NAME="/tmp/log/super-linter.log" \
   --env-file ".github/super-linter.env" \
+  --volume "${PWD}":/tmp/log \
   --volume "${PWD}":/tmp/lint \
   ghcr.io/super-linter/super-linter:slim-v5
