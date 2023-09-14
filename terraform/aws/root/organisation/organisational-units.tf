36a71e6affacdee480c992ad826c30e54a4f80e5
Jacob Woffenden <jacob@woffenden.io>
2023-09-14 09:57:14 +0000

    Add Cloud Platform tenant OU
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/aws/root/organisation/organisational-units.tf
+++ terraform/aws/root/organisation/organisational-units.tf
@@ -4,6 +4,12 @@ module "organisational_unit_cloud_platform_core" {
   parent_id = module.organisation.root_id
 }
 
+module "organisational_unit_cloud_platform_tenants" {
+  source    = "../../../modules/aws/organisation/modules/organisational-unit"
+  name      = "cloud-platform-tenants"
+  parent_id = module.organisation.root_id
+}
+
 module "organisational_unit_cloud_platform_graveyard" {
   source    = "../../../modules/aws/organisation/modules/organisational-unit"
   name      = "cloud-platform-graveyard"
