ad9ab2688d4895e1630b4d5d9da4eb226af12614
Jacob Woffenden <jacob@woffenden.io>
2023-09-30 20:35:07 +0000

    Upgrade AWS provider Comment out Cloud Platform
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/aws/root/organisation/cloud-platform-environments.tf
+++ terraform/aws/root/organisation/cloud-platform-environments.tf
@@ -1,3 +1,4 @@
+/* coming soon
 module "cloud_platform_environments" {
   for_each = {
     for f in fileset(path.module, "configuration/cloud-platform-environments/*.json") : trimsuffix(basename(f), ".json") => f
@@ -8,3 +9,4 @@ module "cloud_platform_environments" {
   configuration       = jsondecode(file("${path.module}/${each.value}"))
   organisational_unit = module.organisational_unit_cloud_platform_tenants.id
 }
+*/
