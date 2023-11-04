ed2a1b9ca0a5448af5f784665f32a33ed9a40b1a
Jacob Woffenden <jacob@woffenden.io>
2023-11-04 21:01:39 +0000

    Updated SSO function
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/aws/root/sso/lambda-functions.tf
+++ terraform/aws/root/sso/lambda-functions.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "aws_sso_sync" {
   #ts:skip=AC_AWS_0484
   function_name = "aws-sso-sync"
   package_type  = "Image"
-  image_uri     = "126246520815.dkr.ecr.eu-west-2.amazonaws.com/aws-ssosync:2.0.3.1"
+  image_uri     = "126246520815.dkr.ecr.eu-west-2.amazonaws.com/aws-ssosync:2.0.3.2"
   role          = aws_iam_role.aws_sso_sync.arn
   timeout       = 15
 
@@ -15,7 +15,6 @@ resource "aws_lambda_function" "aws_sso_sync" {
       AWS_IDENTITY_STORE_REGION       = data.aws_region.current.name
       AWS_SSO_SYNC_GOOGLE_ADMIN       = data.aws_secretsmanager_secret_version.aws_sso_sync_google_admin.secret_string
       AWS_SSO_SYNC_GOOGLE_CREDENTIALS = data.aws_secretsmanager_secret_version.aws_sso_sync_google_credentials.secret_string
-      AWS_SSO_SYNC_GROUP_MATCH        = "email:aws*"
       AWS_SSO_SYNC_LOG_LEVEL          = "debug"
       AWS_SSO_SYNC_SCIM_ENDPOINT      = data.aws_secretsmanager_secret_version.aws_sso_sync_scim_endpoint.secret_string
       AWS_SSO_SYNC_SCIM_TOKEN         = data.aws_secretsmanager_secret_version.aws_sso_sync_scim_token.secret_string
