91eb42fdd6f1421c7e38cfef24fb8dd6a20e7f1c
Jacob Woffenden <jacob@woffenden.io>
2023-11-13 22:03:33 +0000

    🚚 Move GitHub Team assignment (#381)
    Signed-off-by: Jacob Woffenden <jacob@woffenden.io>

--- terraform/github/container-platform.tf
+++ terraform/github/container-platform.tf
@@ -10,3 +10,9 @@ resource "github_team_membership" "jacobwoffenden_container_platform" {
   username = "jacobwoffenden"
   role     = "member"
 }
+
+resource "github_team_repository" "container_platform" {
+  team_id    = github_team.container_platform.id
+  repository = "infrastructure"
+  permission = "push"
+}
