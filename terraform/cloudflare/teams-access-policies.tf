bb96fd8edb9fca11e2fdc034d2fdd2607d9efa2b
Jacob Woffenden <jacob@woffenden.io>
2023-12-29 16:10:51 +0000

    Add `session_duration` flag
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/cloudflare/teams-access-policies.tf
+++ terraform/cloudflare/teams-access-policies.tf
@@ -1,9 +1,10 @@
 resource "cloudflare_access_policy" "paperless_woffenden_family" {
-  account_id     = data.google_secret_manager_secret_version.cloudflare_account_id.secret_data
-  application_id = cloudflare_access_application.paperless_woffenden_family.id
-  name           = "allow"
-  precedence     = "1"
-  decision       = "allow"
+  account_id       = data.google_secret_manager_secret_version.cloudflare_account_id.secret_data
+  application_id   = cloudflare_access_application.paperless_woffenden_family.id
+  name             = "allow"
+  precedence       = "1"
+  decision         = "allow"
+  session_duration = "24h"
 
   include {
     group = [cloudflare_access_group.google_group_home.id]
@@ -11,11 +12,12 @@ resource "cloudflare_access_policy" "paperless_woffenden_family" {
 }
 
 resource "cloudflare_access_policy" "unifi_woffenden_net" {
-  account_id     = data.google_secret_manager_secret_version.cloudflare_account_id.secret_data
-  application_id = cloudflare_access_application.unifi_woffenden_net.id
-  name           = "allow"
-  precedence     = "1"
-  decision       = "allow"
+  account_id       = data.google_secret_manager_secret_version.cloudflare_account_id.secret_data
+  application_id   = cloudflare_access_application.unifi_woffenden_net.id
+  name             = "allow"
+  precedence       = "1"
+  decision         = "allow"
+  session_duration = "24h"
 
   include {
     group = [cloudflare_access_group.google_group_ddat_sre.id]
