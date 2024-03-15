56c47ab60a0de63eda5f40fcddd1f820d2bbbd85
Jacob Woffenden <jacob@woffenden.io>
2024-03-15 16:01:31 +0000

    Add GitHub repository module and configuration
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/github/repositories.tf
+++ terraform/github/repositories.tf
@@ -1,26 +1,19 @@
-resource "github_repository" "infrastructure" {
-  name = "infrastructure"
+module "infrastructure_repository" {
+  source = "../modules/github/repository"
 
-  allow_auto_merge            = true
-  allow_update_branch         = true
-  delete_branch_on_merge      = true
-  has_downloads               = false
-  has_issues                  = true
-  has_projects                = true
-  has_wiki                    = false
-  homepage_url                = "https://woffenden.github.io/infrastructure/"
-  merge_commit_message        = "PR_TITLE"
-  merge_commit_title          = "MERGE_MESSAGE"
-  squash_merge_commit_message = "COMMIT_MESSAGES"
-  squash_merge_commit_title   = "PR_TITLE"
-  vulnerability_alerts        = true
-  web_commit_signoff_required = true
-
-  pages {
-    build_type = "workflow"
-    source {
-      branch = "main"
-      path   = "/"
-    }
+  name                                              = "infrastructure"
+  description                                       = null
+  use_template                                      = false
+  has_projects                                      = true
+  has_wiki                                          = false
+  homepage_url                                      = "https://woffenden.github.io/infrastructure/"
+  pages_enabled                                     = true
+  branch_protection_required_status_checks_contexts = ["Super-Linter"]
+  access = {
+    admins = [module.cloud_platform_team.id]
+    pushers = [
+      module.container_platform_team.id,
+      module.observability_platform_team.id
+    ]
   }
 }
---
+++ terraform/modules/github/repository/main.tf
@@ -0,0 +1,132 @@
+resource "github_repository" "this" {
+  name        = var.name
+  description = var.description
+  topics      = var.topics
+  visibility  = var.visibility
+
+  archived           = var.archived
+  archive_on_destroy = var.archive_on_destroy
+
+  is_template = var.is_template
+
+  dynamic "template" {
+    for_each = var.use_template ? [1] : []
+    content {
+      owner      = "woffenden"
+      repository = var.template_repository
+    }
+  }
+
+  has_discussions      = var.has_discussions
+  has_downloads        = var.has_downloads
+  has_issues           = var.has_issues
+  has_projects         = var.has_projects
+  has_wiki             = var.has_wiki
+  homepage_url         = var.homepage_url
+  vulnerability_alerts = var.vulnerability_alerts
+
+  auto_init = var.auto_init
+
+  web_commit_signoff_required = var.web_commit_signoff_required
+
+  allow_merge_commit   = var.allow_merge_commit
+  merge_commit_title   = var.merge_commit_title
+  merge_commit_message = var.merge_commit_message
+
+  allow_squash_merge          = var.allow_squash_merge
+  squash_merge_commit_title   = var.squash_merge_commit_title
+  squash_merge_commit_message = var.squash_merge_commit_message
+
+  allow_update_branch    = var.allow_update_branch
+  allow_auto_merge       = var.allow_auto_merge
+  allow_rebase_merge     = var.allow_rebase_merge
+  delete_branch_on_merge = var.delete_branch_on_merge
+
+  dynamic "pages" {
+    for_each = var.pages_enabled ? [1] : []
+    content {
+      build_type = "workflow"
+      cname      = var.pages_configuration.cname
+      source {
+        branch = var.pages_configuration.source.branch
+        path   = var.pages_configuration.source.path
+      }
+    }
+  }
+
+  security_and_analysis {
+    dynamic "advanced_security" {
+      for_each = var.visibility == "private" || var.visibility == "internal" ? [1] : []
+      content {
+        status = var.advanced_security_status
+      }
+    }
+
+    secret_scanning {
+      status = var.secret_scanning_status
+    }
+
+    secret_scanning_push_protection {
+      status = var.secret_scanning_push_protection_status
+    }
+  }
+}
+
+resource "github_branch_protection" "this" {
+  repository_id = github_repository.this.id
+  pattern       = var.branch_protection_pattern
+
+  allows_deletions                = var.branch_protection_allows_deletions
+  enforce_admins                  = var.branch_protection_enforce_admins
+  force_push_bypassers            = var.branch_protection_force_push_bypassers
+  require_signed_commits          = var.branch_protection_require_signed_commits
+  required_linear_history         = var.branch_protection_required_linear_history
+  require_conversation_resolution = var.branch_protection_require_conversation_resolution
+  allows_force_pushes             = var.branch_protection_allows_force_pushes
+  lock_branch                     = var.branch_protection_lock_branch
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = var.branch_protection_required_pull_request_reviews_dismiss_stale_reviews
+    restrict_dismissals             = var.branch_protection_required_pull_request_reviews_restrict_dismissals
+    dismissal_restrictions          = var.branch_protection_required_pull_request_reviews_dismissal_restrictions
+    pull_request_bypassers          = var.branch_protection_required_pull_request_reviews_pull_request_bypassers
+    require_code_owner_reviews      = var.branch_protection_required_pull_request_reviews_require_code_owner_reviews
+    require_last_push_approval      = var.branch_protection_required_pull_request_reviews_require_last_push_approval
+    required_approving_review_count = var.branch_protection_required_pull_request_reviews_required_approving_review_count
+  }
+
+  required_status_checks {
+    strict   = var.branch_protection_required_status_checks_strict
+    contexts = var.branch_protection_required_status_checks_contexts
+  }
+}
+
+resource "github_repository_dependabot_security_updates" "this" {
+  repository = github_repository.this.id
+
+  enabled = var.dependabot_security_updates_enabled
+}
+
+resource "github_team_repository" "admin" {
+  for_each = var.access != null && var.access.admins != null ? { for team in var.access.admins : team => team } : {}
+
+  team_id    = each.value
+  repository = github_repository.this.name
+  permission = "admin"
+}
+
+resource "github_team_repository" "maintainers" {
+  for_each = var.access != null && var.access.maintainers != null ? { for team in var.access.maintainers : team => team } : {}
+
+  team_id    = each.value
+  repository = github_repository.this.name
+  permission = "maintain"
+}
+
+resource "github_team_repository" "pushers" {
+  for_each = var.access != null && var.access.pushers != null ? { for team in var.access.pushers : team => team } : {}
+
+  team_id    = each.value
+  repository = github_repository.this.name
+  permission = "push"
+}
