e7d57c64cee4f61766c4b37050b53da0db897530
Jacob Woffenden <jacob@woffenden.io>
2024-03-16 14:00:15 +0000

    :octocat: Add template-repository (#591)
    Signed-off-by: Jacob Woffenden <jacob@woffenden.io>

--- terraform/github/repositories.tf
+++ terraform/github/repositories.tf
@@ -4,7 +4,7 @@ module "infrastructure_repository" {
   source = "../modules/github/repository"
 
   name                                              = "infrastructure"
-  description                                       = null
+  description                                       = "Infrastructure repository"
   use_template                                      = false
   has_projects                                      = true
   has_wiki                                          = false
@@ -17,5 +17,23 @@ module "infrastructure_repository" {
       module.container_platform_team.id,
       module.observability_platform_team.id
     ]
+  }
+}
+
+module "template_repository" {
+  #ts:skip=accurics.github.IAM.1
+
+  source = "../modules/github/repository"
+
+  name                                              = "template-repository"
+  description                                       = "Template repository"
+  is_template                                       = true
+  use_template                                      = false
+  has_projects                                      = false
+  has_wiki                                          = false
+  homepage_url                                      = null
+  branch_protection_required_status_checks_contexts = ["Super-Linter"]
+  access = {
+    admins = [module.cloud_platform_team.id]
   }
 }
