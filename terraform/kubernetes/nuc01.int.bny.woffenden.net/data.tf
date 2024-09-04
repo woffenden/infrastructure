ee1ef17f46b1d29fc87a6fc804a20425077571d5
Jacob Woffenden <jacob@woffenden.io>
2024-09-04 23:44:52 +0000

    add actions
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/kubernetes/nuc01.int.bny.woffenden.net/data.tf
+++ terraform/kubernetes/nuc01.int.bny.woffenden.net/data.tf
@@ -9,3 +9,11 @@ data "google_secret_manager_secret_version" "k8s_nuc01_client_certificate_data" 
 data "google_secret_manager_secret_version" "k8s_nuc01_client_key_data" {
   secret = "k8s-nuc01-client-key-data"
 }
+
+###
+
+data "kubernetes_all_namespaces" "allns" {}
+
+output "all-ns" {
+  value = data.kubernetes_all_namespaces.allns.namespaces
+}
