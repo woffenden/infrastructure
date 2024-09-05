e0d48792d773c31c013160dc913ac51abe5ed07e
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-09-05 21:07:33 +0100

    fix

--- terraform/cloudflare/records.tf
+++ terraform/cloudflare/records.tf
@@ -2,42 +2,42 @@ resource "cloudflare_record" "woffenden_cloud_aws_ns1" {
   zone_id = module.woffenden_cloud_cloudflare_zone.id
   name    = "aws"
   type    = "NS"
-  content   = "ns-1682.awsdns-18.co.uk"
+  content = "ns-1682.awsdns-18.co.uk"
 }
 
 resource "cloudflare_record" "woffenden_cloud_aws_ns2" {
   zone_id = module.woffenden_cloud_cloudflare_zone.id
   name    = "aws"
   type    = "NS"
-  content   = "ns-630.awsdns-14.net"
+  content = "ns-630.awsdns-14.net"
 }
 
 resource "cloudflare_record" "woffenden_cloud_aws_ns3" {
   zone_id = module.woffenden_cloud_cloudflare_zone.id
   name    = "aws"
   type    = "NS"
-  content   = "ns-321.awsdns-40.com"
+  content = "ns-321.awsdns-40.com"
 }
 
 resource "cloudflare_record" "woffenden_cloud_aws_ns4" {
   zone_id = module.woffenden_cloud_cloudflare_zone.id
   name    = "aws"
   type    = "NS"
-  content   = "ns-1286.awsdns-32.org"
+  content = "ns-1286.awsdns-32.org"
 }
 
 resource "cloudflare_record" "woffenden_dev_github_pages_challenge" {
   zone_id = module.woffenden_dev_cloudflare_zone.id
   name    = "_github-pages-challenge-woffenden"
   type    = "TXT"
-  content   = "3ff9951c0a51b83dca75b05f205366"
+  content = "3ff9951c0a51b83dca75b05f205366"
 }
 
 resource "cloudflare_record" "woffenden_family_paperless_cname" {
   zone_id = module.woffenden_family_cloudflare_zone.id
   name    = "paperless"
   type    = "CNAME"
-  content   = cloudflare_tunnel.paperless_woffenden_family.cname
+  content = cloudflare_tunnel.paperless_woffenden_family.cname
   proxied = true
 }
 
@@ -46,7 +46,7 @@ resource "cloudflare_record" "woffenden_io_cdn_r2" {
   zone_id = module.woffenden_io_cloudflare_zone.id
   name    = "cdn"
   type    = "R2"
-  content   = "cdn-woffenden-io"
+  content = "cdn-woffenden-io"
   proxied = true
 }
 */
@@ -55,42 +55,42 @@ resource "cloudflare_record" "woffenden_io_github_pages_challenge" {
   zone_id = module.woffenden_io_cloudflare_zone.id
   name    = "_github-pages-challenge-woffenden"
   type    = "TXT"
-  content   = "3e8f0954722b4e4341b822e923bf87"
+  content = "3e8f0954722b4e4341b822e923bf87"
 }
 
 resource "cloudflare_record" "woffenden_io_github_organisation_challenge" {
   zone_id = module.woffenden_io_cloudflare_zone.id
   name    = "_github-challenge-woffenden-organization"
   type    = "TXT"
-  content   = "7714018f85"
+  content = "7714018f85"
 }
 
 resource "cloudflare_record" "woffenden_net_udmp_int_bny" {
   zone_id = module.woffenden_net_cloudflare_zone.id
   name    = "udmp.int.bny"
   type    = "A"
-  content   = "10.100.0.1"
+  content = "10.100.0.1"
 }
 
 resource "cloudflare_record" "woffenden_net_nuc01_int_bny" {
   zone_id = module.woffenden_net_cloudflare_zone.id
   name    = "nuc01.int.bny"
   type    = "A"
-  content   = "10.100.0.50"
+  content = "10.100.0.50"
 }
 
 resource "cloudflare_record" "woffenden_net_managed_network_cloudflare_bny" {
   zone_id = module.woffenden_net_cloudflare_zone.id
   name    = "managed-network.cloudflare.bny"
   type    = "A"
-  content   = "10.100.0.52"
+  content = "10.100.0.52"
 }
 
 resource "cloudflare_record" "woffenden_net_unifi_cname" {
   zone_id = module.woffenden_net_cloudflare_zone.id
   name    = "unifi"
   type    = "CNAME"
-  content   = cloudflare_tunnel.bny_woffenden_net.cname
+  content = cloudflare_tunnel.bny_woffenden_net.cname
   proxied = true
 }
 
@@ -98,7 +98,7 @@ resource "cloudflare_record" "woffenden_net_homeassistant_cname" {
   zone_id = module.woffenden_net_cloudflare_zone.id
   name    = "homeassistant"
   type    = "CNAME"
-  content   = cloudflare_tunnel.bny_woffenden_net.cname
+  content = cloudflare_tunnel.bny_woffenden_net.cname
   proxied = true
 }
 
@@ -106,7 +106,7 @@ resource "cloudflare_record" "woffenden_net_bny_a" {
   zone_id = module.woffenden_net_cloudflare_zone.id
   name    = "bny"
   type    = "A"
-  content   = "1.1.1.1"
+  content = "1.1.1.1"
   comment = "This record is updated by the IP Update script"
 
   lifecycle {
