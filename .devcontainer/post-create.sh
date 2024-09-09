feabf7c427077823d0e5bb067baa3dceb9f7a76c
Jacob Woffenden <jacob@woffenden.io>
2024-09-09 11:19:46 +0000

    install prettier and pre-commit
    Signed-off-by: GitHub <noreply@github.com>

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Dev Container CLI
+npm install --global @devcontainers/cli
+
+# Install Prettier
+npm install --global prettier
+
+# Install pre-commit
+pip install pre-commit
