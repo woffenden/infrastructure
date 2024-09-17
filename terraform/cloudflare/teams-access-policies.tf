6441093b42e7363cc6b5742dd5d60b5843240e7b
Jacob Woffenden <jacob@woffenden.io>
2024-09-17 21:16:38 +0100

    🚇 Add WARP requirement for system applications (#937)
    Signed-off-by: Jacob Woffenden <jacob@woffenden.io>

--- terraform/cloudflare/teams-access-policies.tf
+++ terraform/cloudflare/teams-access-policies.tf
@@ -22,6 +22,10 @@ resource "cloudflare_access_policy" "unifi_woffenden_net" {
   include {
     group = [cloudflare_access_group.google_group_ddat_sre.id]
   }
+
+  require {
+    device_posture = [cloudflare_device_posture_rule.warp.id]
+  }
 }
 
 resource "cloudflare_access_policy" "homebridge_woffenden_net" {
@@ -35,17 +39,26 @@ resource "cloudflare_access_policy" "homebridge_woffenden_net" {
   include {
     group = [cloudflare_access_group.google_group_ddat_sre.id]
   }
+
+  require {
+    device_posture = [cloudflare_device_posture_rule.warp.id]
+  }
 }
 
 resource "cloudflare_access_policy" "ssh_woffenden_net" {
-  account_id       = data.google_secret_manager_secret_version.cloudflare_account_id.secret_data
-  application_id   = cloudflare_access_application.ssh_woffenden_net.id
-  name             = "allow"
-  precedence       = "1"
-  decision         = "allow"
-  session_duration = "24h"
+  account_id                     = data.google_secret_manager_secret_version.cloudflare_account_id.secret_data
+  application_id                 = cloudflare_access_application.ssh_woffenden_net.id
+  name                           = "allow"
+  precedence                     = "1"
+  decision                       = "allow"
+  session_duration               = "24h"
+  purpose_justification_required = true
 
   include {
     group = [cloudflare_access_group.google_group_ddat_sre.id]
+  }
+
+  require {
+    device_posture = [cloudflare_device_posture_rule.warp.id]
   }
 }
