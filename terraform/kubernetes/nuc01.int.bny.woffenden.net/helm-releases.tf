26945cd260bc348588b5e3e691f47332ddcec4f4
Jacob Woffenden <jacob@woffenden.io>
2024-10-12 15:51:13 +0100

    🚰 Patch Kubernetes applications (#994)
    Signed-off-by: Jacob Woffenden <jacob@woffenden.io>

--- terraform/kubernetes/nuc01.int.bny.woffenden.net/helm-releases.tf
+++ terraform/kubernetes/nuc01.int.bny.woffenden.net/helm-releases.tf
@@ -12,7 +12,7 @@ resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = "1.15.3"
+  version    = "1.16.1"
   namespace  = kubernetes_namespace.cert_manager.metadata[0].name
 
   set {
