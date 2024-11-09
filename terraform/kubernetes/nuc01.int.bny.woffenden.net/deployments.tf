9f0370aa21dc5d09f4a614621bdabc0347fa8f56
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-11-09 18:28:33 +0000

    patch k8s things

--- terraform/kubernetes/nuc01.int.bny.woffenden.net/deployments.tf
+++ terraform/kubernetes/nuc01.int.bny.woffenden.net/deployments.tf
@@ -31,7 +31,7 @@ resource "kubernetes_deployment" "cloudflare_teams_doh_proxy" {
         }
         container {
           name              = "doh-proxy"
-          image             = "docker.io/cloudflare/cloudflared:2024.10.0"
+          image             = "docker.io/cloudflare/cloudflared:2024.11.0"
           image_pull_policy = "Always"
           args = [
             "--no-autoupdate",
@@ -227,7 +227,7 @@ resource "kubernetes_deployment" "homebridge" {
         host_network = true
         container {
           name              = "homebridge"
-          image             = "ghcr.io/homebridge/homebridge:2024-10-09"
+          image             = "ghcr.io/homebridge/homebridge:2024-10-25"
           image_pull_policy = "Always"
           env {
             name  = "TZ"
@@ -486,7 +486,7 @@ resource "kubernetes_deployment" "paperless" {
         }
         container {
           name              = "paperless"
-          image             = "ghcr.io/paperless-ngx/paperless-ngx:2.12.1"
+          image             = "ghcr.io/paperless-ngx/paperless-ngx:2.13.4"
           image_pull_policy = "Always"
           env {
             name  = "USERMAP_UID"
@@ -752,7 +752,7 @@ resource "kubernetes_deployment" "paperless_warp_tunnel" {
         }
         container {
           name              = "warp-tunnel"
-          image             = "docker.io/cloudflare/cloudflared:2024.10.0"
+          image             = "docker.io/cloudflare/cloudflared:2024.11.0"
           image_pull_policy = "Always"
           args = [
             "--no-autoupdate",
